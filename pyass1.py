36abfe97da6ae2932f332d02c00c6bd374f950e1
Vaiju1997 <55177111+Vaiju1997@users.noreply.github.com>
2019-09-11 11:35:13 +0530

    Add files via upload

---
+++ pyass1.py
@@ -0,0 +1,9 @@
+n1=float(input("Enter first number: "))
+n2=float(input("Enter second number: "))
+add = print("n1+n2=",n1+n2)
+sub = print("n1-n2=",n1-n2)
+mul = print("n1*n2=",n1*n2)
+if n2==0:
+ print("n1/n2= Error")
+else:
+ div = print("n1/n2=", n1/n2)
